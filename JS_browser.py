2a6a4a5b220dcb5a3014646d2aee30864835c943
Vishok Srikanth <vishoksrikanth@uchicago.edu>
2017-03-07 19:34:44 -0600

    Testing cleaner browser implementation.

---
+++ JS_browser.py
@@ -0,0 +1,188 @@
+# Vishok Srikanth
+# 
+# Python class to browse pages and perform searches where it's needed to 
+# click elements on the page that result in complicated POST and GET requests
+# that requests and similar packages won't handle properly.
+
+# How to Set Up Linux Prerequisites:
+    # install selenium, pyvirtualdisplay
+        # sudo pip3 install selenium pyvirtualdisplay
+    # install xvfb and chromium
+        # sudo apt-get updtae
+        # sudo apt-get install xvfb chromium-browser
+    # install older version of chromedriver manually:
+        # Linux:https://chromedriver.storage.googleapis.com/index.html?path=2.26/
+        # Download and extract the appropriate archive somewhere.
+        # Open a terminal wherever the executable is (or use its full filepath)
+            # sudo mv chromedriver /usr/bin/chromedriver
+
+import os
+import time
+import signal
+import subprocess
+from selenium import webdriver
+from pyvirtualdisplay import Display
+
+class JS_browser:
+
+    def __init__(self, invisible = True, start_link = False, load_time = 10,
+                 verbose = False):
+        '''
+        Open a webbrowser, navigating to a page. This browser can be used to
+        click on elements within the page, which is particularly useful for
+        clicking download links or check boxes and then taking some actions
+        afterwards.
+        '''
+        self._invisible = invisible
+        self._verbose = verbose
+        self._load_time = load_time
+
+        if self._invisible:
+            self._display = Display(visible=0, size=(800, 600))
+            self._display.start()
+        self._driver = webdriver.Chrome()
+        if start_link:
+            self.go_to_page(start_link)
+
+    @property
+    def source(self):
+        '''
+        Return HTML source code of the browser's current page.
+        '''
+        return self._driver.page_source
+
+    @property
+    def capture(self, output_filename, directory = os.getcwd()):
+        '''
+        Screenshot the web browser, saving PNG to "output_filename".png in
+        current directory unless alternative is specified. Only accepts
+        absolute filepaths.
+        '''
+        valid = self._driver.save_screenshot(directory + "/" + output_filename)
+        if self._verbose and valid:
+            print("Saved screenshot successfully.")
+        elif self._verbose:
+            print("Error saving screenshot.")
+   
+
+    def go_to(self, link):
+        '''
+        Within the browser, navigate to the webpage at "link" and allow a delay
+        time for the page to load.
+        '''
+        try:
+            self._driver.get(link)
+            time.sleep(self._load_time)
+            if self._verbose:
+                print("Loaded new page successfully.")
+        except WebDriverException:
+            if self._verbose:
+                print("Loading page failed. Check link is valid.")
+ 
+
+    def click(self, element):
+        '''
+        Within the browser, click an element and allow some time for the page
+        to respond. Specify the element to be clicked via XPath.
+        '''
+        self._find_and_click(element)
+
+
+    def _find_and_click(self, element):
+        try:
+            button = self._driver.find_element_by_xpath(element)
+            button.click()
+            time.sleep(self._load_time)
+        except NoSuchElementException:
+            if self._verbose:
+                print("Couldn't locate element. Maybe increase loading time?")
+
+
+    class TimeExceededError(Exception):
+        '''
+        A simple error class to handle optional time-outs (see below).
+        '''
+        pass
+
+
+    def download(self, element, default_save_path, output_path = None,
+                 max_time = 600):
+        '''
+        Within the browser, click a download link to download some file,
+        preventing user input until the download is complete. Optionally
+        attempt to move the completed download file to a location specified
+        by the user. "default_save_path" is the filepath the browser downloads
+        to by default (e.g., Chromium on the Linux VMs saves downloads to
+        "/home/student/Downloads/" so if we wish to click a link that downloads
+        a file called "Awards.xml" the default_save_path should be
+        "/home/student/Downloads/Awards.xml"; unfortunately this has to be
+        empirical). By default this is set to timeout downloads after 10
+        minutes, which can help avoid infinite loops caused by mistyping file
+        paths.
+        '''
+        # Default save path occupied. foo.txt will be downloaded as foo(1).txt,
+        # etc. depending on how many similarly-named files are in the default
+        # download location.
+        if os.path.exists(default_save_path):
+            try_name = default_save_path.replace(".", "({}).")
+            i = 1
+            while os.path.exists(try_name.format(i)):
+                i += 1
+            default_save_path = try_name
+
+        if max_time:
+            if not isinstance(max_time, (int, float)):
+                if self._verbose:
+                    print("Error: Invalid timeout specification. Must be a "
+                          "number to be used as maximum allowable seconds to "
+                          "try download process.")
+            self._find_and_click(element)
+            self._timeout_download(default_save_path, max_time)
+        else:
+            self._find_and_click(element)
+            self._download(default_save_path)
+
+        if output_path:
+            bash_command = "mv " + default_save_path + " " + output_path
+            # If there's an issue with the specified output path, an error
+            # gets printed to the terminal automatically.
+            subprocess.Popen(bash_command, shell = True)
+
+
+    def _timeout_download(self, filepath, time_allowed):
+        '''
+        Check if a download has successfully completed, enforcing a time-o
+        '''
+        # Based on suggestions from 
+        # http://stackoverflow.com/questions/492519/timeout-on-a-function-call
+        def handler(signum, frame):
+            raise TimeExceededError
+        timeout_handler = signal.signal(signal.SIGALRM, handler)
+        try:
+            alarm = signal.alarm(time_allowed)
+            self._download(filepath)
+            alarm = signal.alarm(0)
+        except TimeExceededError:
+            if self._verbose:
+                print("Download Failed: Maximum download time exceeded.\n"
+                      "If this is unexpected, ensure file paths were entered "
+                      "properly.")
+
+
+    def _download(self, filepath):
+        '''
+        Check if a download has successfully completed. This WILL hang if the
+        file is specified improperly.
+        '''
+        while not os.path.exists(filepath):
+            time.sleep(10)
+
+
+    def cleanup(self):
+        '''
+        Close the browser (and stop the virtual display, if the browser is
+        running hidden).
+        '''
+        self._driver.quit()
+        if self._invisible:
+            self._display.stop()
--- dl_from_JS_page_nsf.py
+++ dl_from_JS_page_nsf.py
@@ -4,6 +4,7 @@
     # install selenium, pyvirtualdisplay
         # sudo pip3 install selenium pyvirtualdisplay
     # install xvfb and chromium
+        # sudo apt-get updtae
         # sudo apt-get install xvfb chromium-browser
     # install older version of chromedriver manually:
         # Linux:https://chromedriver.storage.googleapis.com/index.html?path=2.26/
@@ -11,50 +12,61 @@
         # Open a terminal wherever the executable is (or use its full filepath)
             # sudo mv chromedriver /usr/bin/chromedriver
 
-import os
-import time
-import subprocess
-from selenium import webdriver
-from pyvirtualdisplay import Display
+from JS_browser import JS_browser
 
 link_to_visit = "https://www.nsf.gov/awardsearch/advancedSearchResult?PIId&PIFirstName&PILastName&PIOrganization&PIState&PIZip&PICountry&ProgOrganization&ProgEleCode&BooleanElement=All&ProgRefCode&BooleanRef=All&Program&ProgOfficer&Keyword=retina%20connectomics&AwardNumberOperator&AwardAmount&AwardInstrument&ActiveAwards=true&OriginalAwardDateOperator&StartDateOperator&ExpDateOperator"
+xml_download_element = '//*[@id="x-auto-32"]/a'
+default_save_path = "/home/student/Downloads/Awards.xml"
+output_path = "/media/sf_VM_Folder/Awards.xml"
 
-display = Display(visible=0, size=(800, 600))
-display.start()
+browser = JS_browser(start_link = link_to_visit)
+browser.download(xml_download_element, default_save_path, output_path)
+browser.cleanup()
 
-driver = webdriver.Chrome()
-driver.get(link_to_visit)
-# Wait long enough for the page to finish loading elements. Maybe change this
-# later to wait for some workpiece indicator to appear, but for now this works.
-# This is hackeneyed, but the page loads the download button before everything
-# else...
-time.sleep(10)
-# input("Please press Enter once the browser shows the page is finished downloading.")
-# Get the xpath manually by inspecting element in Chrome and right click ->
-# copy -> xpath
-download_link = driver.find_element_by_xpath('//*[@id="x-auto-30"]/a')
-# Downloads the file. The default directory is the "Downloads" directory,
-# I'm sure there's a way to control it but idk how yet. Anyway, at least it's
-# predictable.
-download_link.click()
+# import os
+# import time
+# import subprocess
+# from selenium import webdriver
+# from pyvirtualdisplay import Display
 
-# Wait for the download to finish.
-# while not os.path exists("/Users/Vishok/Downloads/Awards.csv"):     # OS X
-while not os.path.exists("/home/student/Downloads/Awards.csv"):       # Linux
-    time.sleep(10)
+# link_to_visit = "https://www.nsf.gov/awardsearch/advancedSearchResult?PIId&PIFirstName&PILastName&PIOrganization&PIState&PIZip&PICountry&ProgOrganization&ProgEleCode&BooleanElement=All&ProgRefCode&BooleanRef=All&Program&ProgOfficer&Keyword=retina%20connectomics&AwardNumberOperator&AwardAmount&AwardInstrument&ActiveAwards=true&OriginalAwardDateOperator&StartDateOperator&ExpDateOperator"
 
-# Visible Browser Alternative
-# input("\nPlease press Enter once the browser shows the file is finished downloading.\n")
+# display = Display(visible=0, size=(800, 600))
+# display.start()
 
-# Clean Up.
-driver.quit()
-display.stop()
+# driver = webdriver.Chrome()
+# driver.get(link_to_visit)
+# # Wait long enough for the page to finish loading elements. Maybe change this
+# # later to wait for some workpiece indicator to appear, but for now this works.
+# # This is hackeneyed, but the page loads the download button before everything
+# # else...
+# time.sleep(10)
+# # input("Please press Enter once the browser shows the page is finished downloading.")
+# # Get the xpath manually by inspecting element in Chrome and right click ->
+# # copy -> xpath
+# download_link = driver.find_element_by_xpath('//*[@id="x-auto-30"]/a')
+# # Downloads the file. The default directory is the "Downloads" directory,
+# # I'm sure there's a way to control it but idk how yet. Anyway, at least it's
+# # predictable.
+# download_link.click()
 
-filepath = input("\nPlease input a destination directory and name for the "
-                 "downloaded CSV file, (no quotation marks, spaces, etc. but "
-                 "including '.csv' if desired) or nothing if you wish to move "
-                 "it manually.\n")
-if filepath:
-    # bash_command = "mv /Users/Vishok/Downloads/Awards.csv " + filepath
-    bash_command = "mv /home/student/Downloads/Awards.csv " + filepath
-    subprocess.Popen(bash_command, shell = True)
+# # Wait for the download to finish.
+# # while not os.path exists("/Users/Vishok/Downloads/Awards.csv"):     # OS X
+# while not os.path.exists("/home/student/Downloads/Awards.csv"):       # Linux
+#     time.sleep(10)
+
+# # Visible Browser Alternative
+# # input("\nPlease press Enter once the browser shows the file is finished downloading.\n")
+
+# # Clean Up.
+# driver.quit()
+# display.stop()
+
+# filepath = input("\nPlease input a destination directory and name for the "
+#                  "downloaded CSV file, (no quotation marks, spaces, etc. but "
+#                  "including '.csv' if desired) or nothing if you wish to move "
+#                  "it manually.\n")
+# if filepath:
+#     # bash_command = "mv /Users/Vishok/Downloads/Awards.csv " + filepath
+#     bash_command = "mv /home/student/Downloads/Awards.csv " + filepath
+#     subprocess.Popen(bash_command, shell = True)
